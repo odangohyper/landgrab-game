f35fa6e3095a2df406836038068a304e8929eb60
odangohyper <heavyjavelin1005yen@gmail.com>
2025-08-16 15:20:08 +0900

    TODO.mdの修正

--- packages/api-server/app/game/cards/gain_funds.py
+++ packages/api-server/app/game/cards/gain_funds.py
@@ -4,6 +4,6 @@ from app.game.models import PlayerState
 def apply_gain_funds(player: PlayerState) -> None:
     """
     「資金集め」カードの効果を適用します。
-    資金が2増加します。
+    資金が1増加します。
     """
     player.funds += 2
